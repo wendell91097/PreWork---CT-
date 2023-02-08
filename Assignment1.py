1615d35eabce2339c0e67c1e5e1f2cee6fbf44d0
wendell91097 <124644341+wendell91097@users.noreply.github.com>
2023-02-08 10:40:01 -0500

    Update Assignment1.py
    Question numbers were labeled incorrectly, question 1 was revised.

--- Assignment1.py
+++ Assignment1.py
@@ -1,9 +1,9 @@
 # Question 1
 # Write a function to print "hello_USERNAME!"
-def hello_name(user_name):
-    print("hello_" + user_name.upper())
+def hello_name():
+    print("hello_USERNAME")
 
-hello_name("username")
+hello_name()
 
 # Question 2
 # Write a python function that prints odd numbers from 1-100 but returns nothing
@@ -11,11 +11,11 @@ def first_odds():
     for number in range(100):
         if number % 2 == 1:
             print(number)
-# Question 3
+
 first_odds()
 print(first_odds())
 
-# Question 4
+# Question 3
 # Please write a Python function, 
 # max_num_in_list to return the max number of a given list. 
 def max_num_in_list(list):
@@ -24,7 +24,8 @@ def max_num_in_list(list):
 
 list1 = [1,2,3,7652,2,5467,23243,3,442452,3]
 print(max_num_in_list(list1))   
-# Question 5
+
+# Question 4
 # Write a function to return if the given year is a leap year. 
 # A leap year is divisible by 4, but not divisible by 100, unless it is also divisible by 400. The return should be boolean Type (true/false).
 def is_leap_year(year):
@@ -41,6 +42,7 @@ def is_leap_year(year):
 
 print(is_leap_year(1997))
 
+# Question 5
 # Write a function to check to see if all numbers in list are consecutive numbers.
 # For example, [2,3,4,5,6,7] are consecutive numbers, but [1,2,4,5] are not consecutive numbers. The return should be boolean Type.
 
